75ca553f92026ef569fc75e321f861436595d24c
LeeJaeHoon <dlwogns3413@naver.com>
2024-11-06 14:52:27 +0900

    feat: Product 수량 음수 안되게 예외 처리

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -8,9 +8,16 @@ public class Product {
     private final Promotion promotion;
 
     public Product(String name, int price, int quantity, Promotion promotion) {
+        validateQuantityIsNegative(quantity);
         this.name = name;
         this.price = price;
         this.quantity = quantity;
         this.promotion = promotion;
+    }
+
+    private void validateQuantityIsNegative(int quantity) {
+        if (quantity < 0) {
+            throw new IllegalArgumentException("수량은 음수가 될 수 없습니다.");
+        }
     }
 }
--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -1,8 +1,11 @@
 package store.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class ProductTest {
 
@@ -21,5 +24,19 @@ class ProductTest {
         assertThat(product)
                 .extracting("name", "price", "quantity", "promotion")
                 .containsExactly(name, price, quantity, promotion);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"-1", "-100", "-20"})
+    void 상품의_수량은_음수가_될_수_없다(int quantity) {
+        //given
+        String name = "콜라";
+        int price = 1000;
+        Promotion promotion = new Promotion();
+
+        //when //then
+        assertThatThrownBy(() -> new Product(name, price, quantity, promotion))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("수량은 음수가 될 수 없습니다.");
     }
 }
