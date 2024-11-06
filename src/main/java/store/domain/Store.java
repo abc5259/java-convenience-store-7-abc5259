eec2ceac82767f2c2235c81924402ec789e813e7
LeeJaeHoon <dlwogns3413@naver.com>
2024-11-06 12:39:55 +0900

    feat: Store 상품 등록시 재고가 음수일 경우 예외 처리

--- src/main/java/store/domain/Store.java
+++ src/main/java/store/domain/Store.java
@@ -7,6 +7,18 @@ public class Store {
     private final Map<Product, Integer> products;
 
     public Store(Map<Product, Integer> products) {
+        validateProductCountIsNotNegative(products);
         this.products = products;
+    }
+
+    private void validateProductCountIsNotNegative(Map<Product, Integer> products) {
+        if (isExistsNegativeCountProduct(products)) {
+            throw new IllegalArgumentException("상품의 재고는 음수가 될 수 없습니다.");
+        }
+    }
+
+    private boolean isExistsNegativeCountProduct(Map<Product, Integer> products) {
+        return products.values().stream()
+                .anyMatch(count -> count < 0);
     }
 }
--- src/test/java/store/domain/StoreTest.java
+++ src/test/java/store/domain/StoreTest.java
@@ -1,6 +1,7 @@
 package store.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Map;
 import org.junit.jupiter.api.Test;
@@ -12,8 +13,8 @@ class StoreTest {
         //given
         Map<Product, Integer> products = Map.of(
                 new Product("콜라", 1000, new Promotion()), 10,
-                new Product("콜라", 1000, null), 9,
-                new Product("콜라", 1000, new Promotion()), 3
+                new Product("오렌지주스", 1000, null), 9,
+                new Product("비타민워터", 1000, new Promotion()), 3
         );
 
         //when
@@ -22,5 +23,20 @@ class StoreTest {
         //then
         assertThat(store).extracting("products")
                 .isEqualTo(products);
+    }
+
+    @Test
+    void 등록되는_상품의_재고는_음수가_될_수_없다() {
+        //given
+        Map<Product, Integer> products = Map.of(
+                new Product("콜라", 1000, new Promotion()), 10,
+                new Product("오렌지주스", 1000, null), -1,
+                new Product("비타민워터", 1000, new Promotion()), 3
+        );
+
+        //when //then
+        assertThatThrownBy(() -> new Store(products))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상품의 재고는 음수가 될 수 없습니다.");
     }
 }
