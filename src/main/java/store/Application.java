4ca861bf9c2b7bd5472f9eab07c09ec4d70c4726
LeeJaeHoon <dlwogns3413@naver.com>
2024-11-10 15:06:51 +0900

    feat: 프로그램 예외 처리 추가

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -5,9 +5,14 @@ import store.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        ConvenienceSystemRunner convenienceSystemRunner = new ConvenienceSystemRunner(inputView, outputView);
-        convenienceSystemRunner.run();
+        try {
+            InputView inputView = new InputView();
+            OutputView outputView = new OutputView();
+            ConvenienceSystemRunner convenienceSystemRunner = new ConvenienceSystemRunner(inputView, outputView);
+            convenienceSystemRunner.run();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 }
