cdb31acada0d7efbdfaaae1c138265715ef48a58
LeeJaeHoon <dlwogns3413@naver.com>
2024-11-10 15:19:40 +0900

    fix: 프로그램 예외 처리 제거

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -5,14 +5,9 @@ import store.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            InputView inputView = new InputView();
-            OutputView outputView = new OutputView();
-            ConvenienceSystemRunner convenienceSystemRunner = new ConvenienceSystemRunner(inputView, outputView);
-            convenienceSystemRunner.run();
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-//            throw new IllegalArgumentException(e.getMessage());
-        }
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        ConvenienceSystemRunner convenienceSystemRunner = new ConvenienceSystemRunner(inputView, outputView);
+        convenienceSystemRunner.run();
     }
 }
