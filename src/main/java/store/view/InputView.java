3948096b8294b991b399b59190b3dc3099a994bb
LeeJaeHoon <dlwogns3413@naver.com>
2024-11-11 13:03:41 +0900

    refactor: view 메시지 상수화

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -6,6 +6,10 @@ import store.domain.PromotionNoticeType;
 
 public class InputView {
     private static final String INPUT_PURCHASE_ITEMS_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private static final String INPUT_EXTRA_PROMOTION_QUANTITY_FORMAT = "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n";
+    private static final String INPUT_NOT_APPLIED_PROMOTION_QUANTITY_FORMAT = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n";
+    private static final String INPUT_MEMBERSHIP_DISCOUNT_ANSWER = "멤버십 할인을 받으시겠습니까? (Y/N)";
+    private static final String INPUT_ADDITIONAL_PURCHASE_ANSWER = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
 
     public String inputPurchaseItems() {
         System.out.println(INPUT_PURCHASE_ITEMS_MESSAGE);
@@ -14,15 +18,25 @@ public class InputView {
 
     public String inputExtraPromotionNoticeRequest(PromotionNoticeResult promotionNoticeResult) {
         System.out.println();
+        return inputExtraPromotionNoticeResponse(promotionNoticeResult);
+    }
+
+    public String inputMembershipDiscountRequest() {
+        System.out.println();
+        System.out.println(INPUT_MEMBERSHIP_DISCOUNT_ANSWER);
+        return Console.readLine();
+    }
+
+    private String inputExtraPromotionNoticeResponse(PromotionNoticeResult promotionNoticeResult) {
         if (promotionNoticeResult.promotionNoticeType() == PromotionNoticeType.MORE_QUANTITY) {
-            System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n",
+            System.out.printf(INPUT_EXTRA_PROMOTION_QUANTITY_FORMAT,
                     promotionNoticeResult.productName(),
                     promotionNoticeResult.productQuantity());
             return Console.readLine();
         }
 
         if (promotionNoticeResult.promotionNoticeType() == PromotionNoticeType.NOT_APPLIED_QUANTITY) {
-            System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n",
+            System.out.printf(INPUT_NOT_APPLIED_PROMOTION_QUANTITY_FORMAT,
                     promotionNoticeResult.productName(),
                     promotionNoticeResult.productQuantity());
             return Console.readLine();
@@ -31,14 +45,8 @@ public class InputView {
         throw new IllegalArgumentException("추가 요청 사항이 없습니다.");
     }
 
-    public String inputMembershipDiscountRequest() {
-        System.out.println();
-        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
-        return Console.readLine();
-    }
-
     public String inputMorePurchaseProducts() {
-        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+        System.out.println(INPUT_ADDITIONAL_PURCHASE_ANSWER);
         String answer = Console.readLine();
         System.out.println();
         return answer;
--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -9,14 +9,21 @@ import store.domain.Receipt;
 import store.domain.Store;
 
 public class OutputView {
+    private static final String START_MESSAGE = "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.";
+    private static final String NO_QUANTITY = "재고 없음";
+    private static final String QUANTITY_UNIT = "개";
+    private static final String RECEIPT_TITLE = "==============W 편의점================";
+    private static final String RECEIPT_GIVEAWAY_TITLE = "=============증\t\t정===============";
+    private static final String RECEIPT_PRICE_TITLE = "====================================";
     private static final String PRODUCT_INFO_FORMAT = "- %s %,d원 %s%n";
     private static final String PROMOTION_PRODUCT_INFO_FORMAT = "- %s %,d원 %s %s%n";
     private static final String ERROR_MESSAGE_FORMAT = "[ERROR] %s 다시 입력해 주세요.%n";
     private static final String RECEIPT_FORMAT = "%-16s\t%-6s\t%-10s%n";
     private static final DecimalFormat decimalFormat = new DecimalFormat("###,###");
+    private static final String MINUS = "-";
 
     public void printStartMessage() {
-        System.out.println("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.");
+        System.out.println(START_MESSAGE);
         printEmptyLine();
     }
 
@@ -35,7 +42,7 @@ public class OutputView {
 
     private void printPurchaseProducts(List<ProductPurchaseLog> productPurchaseLogs) {
         printEmptyLine();
-        System.out.println("==============W 편의점================");
+        System.out.println(RECEIPT_TITLE);
         System.out.printf(RECEIPT_FORMAT, "상품명", "수량", "금액");
         productPurchaseLogs.forEach(purchaseLog -> {
             System.out.printf(RECEIPT_FORMAT,
@@ -46,7 +53,7 @@ public class OutputView {
     }
 
     private void printGiveawayProducts(List<ProductPurchaseLog> productPurchaseLogs) {
-        System.out.println("=============증\t\t정===============");
+        System.out.println(RECEIPT_GIVEAWAY_TITLE);
         productPurchaseLogs.forEach(purchaseLog -> {
             if (purchaseLog.giveawayProductQuantity() == 0) {
                 return;
@@ -57,13 +64,13 @@ public class OutputView {
     }
 
     private void printPrice(Receipt receipt) {
-        System.out.println("====================================");
+        System.out.println(RECEIPT_PRICE_TITLE);
         System.out.printf(RECEIPT_FORMAT, "총구매액", decimalFormat.format(receipt.getTotalQuantity()),
                 decimalFormat.format(receipt.getTotalPrice()));
         System.out.printf(RECEIPT_FORMAT, "행사할인", "",
-                "-" + decimalFormat.format(receipt.getTotalGiveawayProductPrice()));
+                MINUS + decimalFormat.format(receipt.getTotalGiveawayProductPrice()));
         System.out.printf(RECEIPT_FORMAT, "멤버십할인", "",
-                "-" + decimalFormat.format(receipt.getMembershipDiscountPrice()));
+                MINUS + decimalFormat.format(receipt.getMembershipDiscountPrice()));
         System.out.printf(RECEIPT_FORMAT, "내실돈", "", " " + decimalFormat.format(receipt.getLastPrice()));
         printEmptyLine();
     }
@@ -92,8 +99,8 @@ public class OutputView {
 
     private String makeProductQuantityFormat(int quantity) {
         if (quantity == 0) {
-            return "재고 없음";
+            return NO_QUANTITY;
         }
-        return quantity + "개";
+        return quantity + QUANTITY_UNIT;
     }
 }
